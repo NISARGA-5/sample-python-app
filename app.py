51550b87e1419960b7fa8763e52cdd08a88c6502
NISARGA-5 <nisargak148@gmail.com>
2026-07-07 09:35:24 +0000

    Version 5

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Argo CD on GKE! Version 4!"
+    return "Hello from Argo CD on GKE! Version 5!"
 
 @app.route("/health")
 def health():
